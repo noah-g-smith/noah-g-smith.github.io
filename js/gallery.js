02c8dfb1fd72034c6fdc30e0a56f8fb091555093
noah-g-smith <noah@smithsalive.com>
2020-03-03 10:52:49 +1100

    gallery works now

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,33 @@
+// Acticates the image gallery.
+
 function activateGallery() {
-  alert('yo yo yo');
+  let thumbnails = document.querySelectorAll('#gallery-thumbs > div > img');
+  let mainImage = document.querySelector('#gallery-photo > img')
+
+  thumbnails.forEach(function(thumbnail) {
+    // Preload images
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
+    thumbnail.addEventListener('click', function() {
+      // Set clicked image as main image
+
+      mainImage.setAttribute('src', newImageSrc);
+      mainImage.setAttribute('alt', thumbnail.alt);
+
+
+      // Change which image has class 'current'
+      document.querySelector('.current').classList.remove('current');
+      thumbnail.parentNode.classList.add('current');
+
+      // Update image info.
+      let galleryInfo = document.querySelector('#gallery-info');
+      let title = galleryInfo.querySelector('.title');
+      let description = galleryInfo.querySelector('.description');
+
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
