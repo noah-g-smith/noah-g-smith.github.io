cc91c8d1fcb336808f39c88e993fe0ca029ea1b7
noah-g-smith <noah@smithsalive.com>
2020-04-10 17:13:10 +1000

    Updated gallery for projects, and main page.

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -2,32 +2,24 @@
 
 function activateGallery() {
   let thumbnails = document.querySelectorAll('#gallery-thumbs > div > img');
-  let mainImage = document.querySelector('#gallery-photo > img')
+  let mainProject = document.querySelector('.gallery-project');
 
   thumbnails.forEach(function(thumbnail) {
     // Preload images
     let newImageSrc = thumbnail.dataset.largeVersion;
+    let newTitle = thumbnail.dataset.title;
     let largeVersion = new Image();
     largeVersion.src = newImageSrc;
 
     thumbnail.addEventListener('click', function() {
-      // Set clicked image as main image
-
-      mainImage.setAttribute('src', newImageSrc);
-      mainImage.setAttribute('alt', thumbnail.alt);
-
 
       // Change which image has class 'current'
       document.querySelector('.current').classList.remove('current');
       thumbnail.parentNode.classList.add('current');
 
-      // Update image info.
-      let galleryInfo = document.querySelector('#gallery-info');
-      let title = galleryInfo.querySelector('.title');
-      let description = galleryInfo.querySelector('.description');
-
-      title.innerHTML = thumbnail.dataset.title;
-      description.innerHTML = thumbnail.dataset.description;
+      // Change gallery project
+      let path66 = thumbnail.dataset.project;
+      mainProject.setAttribute('src', path66);
     });
   });
 }
